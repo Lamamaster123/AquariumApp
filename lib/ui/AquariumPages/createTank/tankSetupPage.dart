59711d94f058e00bcce8dc5a04ccf646e581dda4
amaljacob <1114778@apps.nsd.org>
2020-11-06 08:48:13 -0800

    Added text fields and began design on tank setup page

--- lib/ui/AquariumPages/createTank/tankSetupPage.dart
+++ lib/ui/AquariumPages/createTank/tankSetupPage.dart
@@ -6,12 +6,62 @@ class TankSetupPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sub Page'),
+        title: Text('Tank Setup'),
         backgroundColor: Colors.blueGrey,
       ),
-      body: Center(
+      body: Container(
+        //alignment: new FractionalOffset(10.0, 1.0),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 40),
+              Text(
+                "Let's start with the basics",
+                style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black87),
+              ),
+              SizedBox(height: 40),
+              Text(
+                "Name",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black54),
+              ),
 
-      ),
-    );
+              TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Enter name'
+                ),
+              ),
+              SizedBox(height: 40),
+              
+              Text(
+                "Size",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black54),
+              ),
+
+              TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Enter size in gallons'
+                ),
+              ),
+              SizedBox(height: 40),
+
+
+
+      ]),
+    ));
   }
 }
