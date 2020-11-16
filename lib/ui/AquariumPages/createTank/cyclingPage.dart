c45a3eff2c1c347df0d179c50c607723b70e0abc
amaljacob <1114778@apps.nsd.org>
2020-11-15 21:51:22 -0800

    Finished designing tank setup page and cycling page, created addFish page

---
+++ lib/ui/AquariumPages/createTank/cyclingPage.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class CyclingPage extends StatefulWidget {
+  @override
+  _CyclingPageState createState() => _CyclingPageState();
+}
+
+class _CyclingPageState extends State<CyclingPage> {
+  bool cycled = false;
+
+  void _onCycled(bool newValue) => setState(() {
+    cycled = newValue;
+  });
+//onPressed: _isButtonDisabled ? null : _incrementCounter,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Cycling'),
+          backgroundColor: Colors.blueGrey,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.arrow_forward),
+              onPressed: () {
+                cycled ? debugPrint("hello") : null;
+                //navigateToSubPage(context);
+              },
+            )
+          ],
+        ),
+        body: Container(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 40),
+                  Container(
+                    alignment: Alignment(0.0, 0.0),
+                    child: Text(
+                      "Have you cycled your tank yet?",
+                      style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black54),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+                  Container(
+                      margin: const EdgeInsets.only(left: 10.0, right: 10.0),
+                      child: Divider(color: Colors.black,)
+                  ),
+
+                  Row(
+                    children: [
+                      SizedBox(width: 50),
+                      Text(
+                        'My tank has cycled for AT LEAST 2 weeks',
+                        style: TextStyle(
+                            fontSize:13.0,
+                            color: Colors.black54),
+                      ),
+
+                      Checkbox(
+                          value: cycled,
+                          onChanged: _onCycled,
+                      ),
+
+
+                    ],
+                  ),
+
+                  Container(
+                      margin: const EdgeInsets.only(left: 10.0, right: 10.0),
+                      child: Divider(color: Colors.black,)
+                  ),
+                  SizedBox(height: 10),
+
+                  Container(
+                    alignment: Alignment(-1.0, 0.0),
+                    margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                    child: Text(
+                      "What is cycling?",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF6E7FAA)),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+
+                  Container(
+                    alignment: Alignment(-1.0, 0.0),
+                    margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                    child: Text(
+                      "Cycling a tank means to establish a colony of the beneficial bacteria that your fish need.",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontSize: 15.0,
+                          color: Color(0xFF6E7FAA)),
+                    ),
+                  ),
+
+                  SizedBox(height: 10),
+
+                  Container(
+                    alignment: Alignment(-1.0, 0.0),
+                    margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                    child: Text(
+                      "When fish waste accumulates, it breaks down into a toxic chemical called ammonia. Beneficial bacteria, however, will process ammonia into nitrites, and nitrites into nitrates, which are far less toxic to your fish. ",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontSize: 15.0,
+                          color: Color(0xFF6E7FAA)),
+                    ),
+                  ),
+
+                  // -------SECOND PARA--------
+                  SizedBox(height: 20),
+
+                  Container(
+                    alignment: Alignment(-1.0, 0.0),
+                    margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                    child: Text(
+                      "How do you cycle a tank?",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF6E7FAA)),
+                    ),
+                  ),
+
+
+
+                  SizedBox(height: 10),
+
+                  Container(
+                    alignment: Alignment(-1.0, 0.0),
+                    margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                    child: Text(
+                      "There are multiple ways to cycle a tank. Detailed steps to most of these options can be found online.",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontSize: 15.0,
+                          color: Color(0xFF6E7FAA)),
+                    ),
+                  ),
+
+                  SizedBox(height: 10),
+
+                  Container(
+                    alignment: Alignment(-1.0, 0.0),
+                    margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                    child: Text(
+                      "In general, after filling the tank with dechlorinated water, a source of ammonia must be added to initially feed the beneficial bacteria.  Afterwards, wait until both ammonia and nitrite levels have stabilized at 0 ppm (parts per million). A test kit is suggested to measure these values.",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontSize: 15.0,
+                          color: Color(0xFF6E7FAA)),
+                    ),
+                  ),
+
+                  SizedBox(height: 10),
+
+                  Container(
+                    alignment: Alignment(-1.0, 0.0),
+                    margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                    child: Text(
+                      "Cycling will typically take anywhere from 2 weeks to 2 months. Once the tank is cycled it is ready for fish to be added.",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontSize: 15.0,
+                          color: Color(0xFF6E7FAA)),
+                    ),
+                  ),
+
+
+
+
+
+
+
+
+
+                ]
+            )
+        )
+    );
+  }
+}
--- lib/ui/AquariumPages/createTank/tankSetupPage.dart
+++ lib/ui/AquariumPages/createTank/tankSetupPage.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TankSetupPage extends StatelessWidget {
+import 'cyclingPage.dart';
+
+class TankSetupPage extends StatefulWidget {
+  @override
+  _TankSetupPageState createState() => _TankSetupPageState();
+}
+
+class _TankSetupPageState extends State<TankSetupPage> {
+  String dropdownValue1 = 'Regular tank';
+  String dropdownValue2 = 'No plants';
+  String dropdownValue3 = 'None';
+
+  Future navigateToSubPage(context) async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => CyclingPage()));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Tank Setup'),
         backgroundColor: Colors.blueGrey,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.arrow_forward),
+            onPressed: () {
+              debugPrint("hello");
+              navigateToSubPage(context);
+            },
+          )
+        ],
       ),
       body: Container(
         //alignment: new FractionalOffset(10.0, 1.0),
@@ -15,15 +39,248 @@ class TankSetupPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               SizedBox(height: 40),
-              Text(
-                "Let's start with the basics",
-                style: TextStyle(
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black87),
+              Container(
+                alignment: Alignment(0.0, 0.0),
+                child: Text(
+                  "Let's start with the basics",
+                  style: TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black54),
+                ),
               ),
-              SizedBox(height: 40),
-              Text(
+
+              SizedBox(height: 20),
+              Center(
+                child: Divider(color: Colors.black),
+              ),
+              SizedBox(height: 10),
+
+
+
+
+              Row(
+                children: [
+                  Text(
+                    "  Name     ",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black54),
+                  ),
+                  Container(
+                    width: 300.0,
+                    alignment: Alignment(1.0, 0.0),
+                    child: TextFormField(
+                      initialValue: 'My New Tank',
+                      decoration: InputDecoration(
+                        labelText: 'Tank name',
+                        border: OutlineInputBorder(),
+                        /*suffixIcon: Icon(
+                          Icons.error,
+                        ),*/
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 20),
+              Center(
+                child: Divider(color: Colors.black),
+              ),
+              SizedBox(height: 10),
+
+              Row(
+                children: [
+                  Text(
+                    "  Size        ",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black54),
+                  ),
+                  Container(
+                    width: 300.0,
+                    alignment: Alignment(1.0, 0.0),
+                    child: TextFormField(
+                      initialValue: 'My New Tank',
+                      decoration: InputDecoration(
+                        labelText: 'Size (gallons)',
+                        border: OutlineInputBorder(),
+                        /*suffixIcon: Icon(
+                          Icons.error,
+                        ),*/
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 20),
+              Center(
+                child: Divider(color: Colors.black),
+              ),
+              SizedBox(height: 10),
+
+
+
+              Row(
+                children: [
+                  Text(
+                    "  Shape",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black54),
+                  ),
+                  SizedBox(width: 180),
+                  DropdownButton<String>(
+                    value: dropdownValue1,
+                    icon: Icon(Icons.arrow_downward),
+                    iconSize: 24,
+                    elevation: 16,
+                    style: TextStyle(color: Colors.blueGrey),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.blueGrey,
+                    ),
+                    onChanged: (String newValue) {
+                      setState(() {
+                        dropdownValue1 = newValue;
+                      });
+                    },
+                    items: <String>['Regular tank', 'Long tank', 'Tall tank    ']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+
+
+              SizedBox(height: 20),
+              Center(
+                child: Divider(color: Colors.black),
+              ),
+              SizedBox(height: 10),
+
+              Row(
+                children: [
+                  Text(
+                    "  Plant Density",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black54),
+                  ),
+                  SizedBox(width: 113),
+                  DropdownButton<String>(
+                    value: dropdownValue2,
+                    icon: Icon(Icons.arrow_downward),
+                    iconSize: 24,
+                    elevation: 16,
+                    style: TextStyle(color: Colors.blueGrey),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.blueGrey,
+                    ),
+                    onChanged: (String newValue) {
+                      setState(() {
+                        dropdownValue2 = newValue;
+                      });
+                    },
+                    items: <String>['No plants', 'Lightly planted', 'Densely planted']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+
+
+              SizedBox(height: 20),
+              Center(
+                child: Divider(color: Colors.black),
+              ),
+              SizedBox(height: 10),
+
+              Row(
+                children: [
+                  Text(
+                    "  Plant Density",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black54),
+                  ),
+                  SizedBox(width: 113),
+                  DropdownButton<String>(
+                    value: dropdownValue3,
+                    icon: Icon(Icons.arrow_downward),
+                    iconSize: 24,
+                    elevation: 16,
+                    style: TextStyle(color: Colors.blueGrey),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.blueGrey,
+                    ),
+                    onChanged: (String newValue) {
+                      setState(() {
+                        dropdownValue3 = newValue;
+                      });
+                    },
+                    items: <String>['None', 'Gravel', 'Sand', 'Garden soil', 'Aqua soil']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+
+
+              SizedBox(height: 20),
+              Center(
+                child: Divider(color: Colors.black),
+              ),
+              SizedBox(height: 10),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            ]),
+    ));
+  }
+}
+
+/*
+Text(
                 "Name",
                 textAlign: TextAlign.left,
                 style: TextStyle(
@@ -86,10 +343,4 @@ class TankSetupPage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 40),
-
-
-
-      ]),
-    ));
-  }
-}
+ */
